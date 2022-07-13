34f447a9391362804ab8cff0979bfb88fd0f967c
Nergis Kuzucu <nergiskuzucu@hotmail.com>
2022-07-13 19:13:53 +0300

    Update daireHesap.java

--- odevler/daireHesap.java
+++ odevler/daireHesap.java
@@ -1,24 +1,38 @@
+/*
+
+Ödev
+Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+
+𝜋 sayısını = 3.14 alınız.
+
+Formül : (𝜋 * (r*r) * 𝛼) / 360
+
+*/
+
 import java.util.Scanner;
 
 public class daireHesap {
     public static void main(String[] args) {
         
-    double  pi=3.14 , r , cevre , alan;
+    double  pi=3.14 , r , cevre , alan , alpha;
 
-    System.out.println("Dairenin Yarı Çapını Giriniz");
+    System.out.println("Dairenin Yarı Çapını Giriniz : ");
     
     Scanner input = new Scanner(System .in);
     
     r = input.nextDouble();
 
-            
-    cevre=2*pi*r;
-    alan = pi*r*r;
     
-    System.out.println("Dairenin Çevresi " +cevre);   
+    System.out.println("Dairenin Merkez Açısının Ölçüsünü Giriniz : ");
+    
+    Scanner input2 = new Scanner(System .in);
+    
+    alpha = input2.nextDouble();
+
+    alan = ((pi*r*r)*alpha)/360;
+     
 
     System.out.println("Dairenin Alanı " +alan);
-    
 
 
     }
